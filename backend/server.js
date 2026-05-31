bcd8fdc647a0e4c29fd411358e0c5125a71856a8
Muhammad Raheel <raheel31102004@gmail.com>
2026-05-31 18:48:13 +0500

    basic server setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import 'dotenv/config.js';
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        succcess: true,
+        message: "api running"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`visit http://localhost:${PORT}`);
+})
